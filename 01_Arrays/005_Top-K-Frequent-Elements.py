2ce99205f50d38176943585833462e1dbade11c2
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-30 18:03:11 -0500

    Create 005_Top-K-Frequent-Elements
    Added problem description and link.

---
+++ 01_Arrays/005_Top-K-Frequent-Elements.py
@@ -0,0 +1,21 @@
+"""
+Problem: Top K Frequent Elements
+Link: https://leetcode.com/problems/top-k-frequent-elements/description/
+
+Given an integer array nums and an integer k, return the k most frequent elements.
+The test cases are generated such that the answer is always unique.
+You may return the answer in any order.
+
+Example 1:
+Input: nums = [1,2,2,3,3,3], k=2
+Output: [2,3]
+
+Example 2:
+Input: nums = [7,7], k=1
+Output: [7]
+
+Constraints:
+1<=nums.length<=10^4
+-1000<=nums[i]<=1000
+1<=k<=number of distinct elements in nums
+"""
