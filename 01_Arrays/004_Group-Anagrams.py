9eeb45098139ebac61a25d1adb48344bdcf49991
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-30 17:38:12 -0500

    Create 004_Group-Anagrams.py and input solution

---
+++ 01_Arrays/004_Group-Anagrams.py
@@ -0,0 +1,36 @@
+"""
+Problem: Group Anagrams
+Link: https://leetcode.com/problems/group-anagrams/description/
+
+Given an array of strings strs, group all anagrams together into sublists. You may return the output in any order.
+An anagram is a string that contains the exact same characters as another string, but the order of the charactesr can be different.
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+"""
