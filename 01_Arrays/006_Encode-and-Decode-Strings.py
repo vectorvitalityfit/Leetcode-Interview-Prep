930da63f84fb4d91fdd2f68d8cb4cde34fcdb2a1
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-30 19:28:33 -0500

    Update 006_Encode-and-Decode-Strings.py
    Added encode and decode methods.

--- 01_Arrays/006_Encode-and-Decode-Strings.py
+++ 01_Arrays/006_Encode-and-Decode-Strings.py
@@ -21,3 +21,29 @@ strs[i] contains any possible characters out of 256 Valid ASCII characters.
 
 Follow-up: Your solution should work for any possible set of characters.
 """
+class Codec:
+    def encode(self, strings):
+        """
+        :type strings: List[str]
+        :rtype: str
+        """
+        # Prefix each string with its length and a delimiter '#'
+        # e.g., ["Hi", ''] -> "2#Hi0#"
+        encoded=[]
+        for s in strings:
+            encoded.append(f"{len(s)}#{s}")
+        return "".join(encoded)
+    
+    def decode(self,s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        result=[]
+        i=0
+        while i<len(s):
+            j=s.find('#',i) # Read Length
+            length=int(s[i:j])
+            result.append(s[j+1:j+1+length]) # Extract the string of that length after the delimiter
+            i=j+1+length # Move pointer past this segment
+        return result
