4153039194a0769ccc7e01513f6a1e84dc984999
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-30 19:02:13 -0500

    Create 006_Encode-and-Decode-Strings.py
    Added problem description and link.

---
+++ 01_Arrays/006_Encode-and-Decode-Strings.py
@@ -0,0 +1,23 @@
+"""
+Problem: Encode and Decode Strings
+Link: https://leetcode.com/problems/encode-and-decode-strings/description/
+
+Design an algorithm to encode a list of strings to a single string. The encoded string is sent over the network and decoded back to the original list of strings.
+
+Implement the encode and decode methods:
+
+Example 1:
+Input: dummy_input=["Hello","World"]
+Output: ["Hello", "World"]
+
+Example 2:
+Input: dummy_input=[""]
+Output: ['']
+
+Constraints:
+0<=str.length<=100
+0<=strs[i].length<200
+strs[i] contains any possible characters out of 256 Valid ASCII characters.
+
+Follow-up: Your solution should work for any possible set of characters.
+"""
