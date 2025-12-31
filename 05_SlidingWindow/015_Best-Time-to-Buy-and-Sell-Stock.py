4d2f0df6f3fa0c3cc59a12baf1757eba15ad4e9b
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-31 02:57:23 -0500

    Create 015_Best-Time-to-Buy-and-Sell-Stock.py
    Added problem description and link.

---
+++ 05_SlidingWindow/015_Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,20 @@
+"""
+Problem: Best Time to Buy and Sell Stock
+Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing one day to buy and a later day to sell.
+Return the maximum profit you can achieve. If no profit is possible, return 0.
+
+Example 1:
+Input: prices=[7,1,5,3,6,4]
+Output: 5
+
+Example 2:
+Input: prices=[7,6,4,3,1]
+Output: 0
+
+Constraints:
+1<=prices.length<=10^5
+0<=prices[i]<=10^4
+"""
