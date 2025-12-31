2ceb79c7c90edd3a28ae2df75b0754609d6d0adc
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-30 20:53:27 -0500

    Create 008_Longest-Consecutive-Sequence.py
    Added problem description and link.

---
+++ 01_Arrays/008_Longest-Consecutive-Sequence.py
@@ -0,0 +1,20 @@
+"""
+Problem: Longest Consecutive Sequence
+Link: https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+Given an array of integers nums, return the length of the longest consecutive sequence of elements that can be formed.
+A consecutive sequence is a sequence where each element is exactly one greater than the previous element.
+Elements do not appear consecutively in the original array.
+
+You must write an algorithm that runs in O(n) time.
+
+Example 1:
+Input: nums=[2,20,4,10,3,4,5]
+Output: 4
+Explanation: The longest consecutive sequence is [2,3,4,5].
+
+Example 2:
+Input: nums=[0,3,2,5,4,6,1,1]
+Output: 7
+
+"""
