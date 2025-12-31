06e8e12fa2d51e617985db55f72f32d11f3f1b24
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-30 20:07:50 -0500

    Create 007_Products-of-Array-Except-Itself.py
    Added problem description and link.

---
+++ 01_Arrays/007_Products-of-Array-Except-Itself.py
@@ -0,0 +1,20 @@
+"""
+Problem: Product of Array Except Itself
+Link: https://leetcode.com/problems/product-of-array-except-itself/description/
+
+Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+Each product fits in a 32-bit integer.
+Follow-up: Solve in O(n) time without using division.
+
+Example 1:
+Input: nums=[1,2,4,6]
+Output: [48,24,12,8]
+
+Example 2:
+Input: nums=[-1,0,1,2,3]
+Output: [0,-6,0,0,0]
+
+Constraints: 
+2<=nums.length<=1000
+-20<=nums[i]<=20
+"""
