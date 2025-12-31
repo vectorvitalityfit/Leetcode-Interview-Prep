bac48cc1c64a70dd46064d8be1e070b88330589c
Sheraz Arshad <vectorvitalityfit@gmail.com>
2025-12-31 11:16:03 -0500

    Create 017_Longest-Repeating-Character-Replacement.py
    Added problem description and link.

---
+++ 05_SlidingWindow/017_Longest-Repeating-Character-Replacement.py
@@ -0,0 +1,10 @@
+"""
+Problem: Longest Repeating Character Replacement
+Link: https://leetcode.com/problems/longest-repeating-character-replacement/description/
+
+Given a string s of uppercase English letters and an integer k, you can replace up to k characters in s
+with any uppercase letter. Return the length of the longest substring containing only one distinct character after
+at most k replacements.
+
+
+"""
