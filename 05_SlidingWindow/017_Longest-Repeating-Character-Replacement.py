e009378a16be89c4db6b1200e359501a8a801e8c
Sheraz Arshad <vectorvitalityfit@gmail.com>
2026-01-04 23:46:28 -0500

    Update 017_Longest-Repeating-Character-Replacement.py
    Added examples to description.

--- 05_SlidingWindow/017_Longest-Repeating-Character-Replacement.py
+++ 05_SlidingWindow/017_Longest-Repeating-Character-Replacement.py
@@ -6,5 +6,17 @@ Given a string s of uppercase English letters and an integer k, you can replace 
 with any uppercase letter. Return the length of the longest substring containing only one distinct character after
 at most k replacements.
 
+Example 1:
+Input: s='XYYX', k=2
+Output: 4
+Explanation: Replace both 'X's with 'Y's or both 'Y's with 'X's to get 'YYYY' or 'XXXX'.
 
+Example 2:
+Input: s='AAABABB', k=1
+Output: 5
+Explanation: Change the 'B' to an 'A' to get 'AAAAABB', longest=5('AAAAA').
+
+Constraints:
+1<=s.length<=1000
+0<=k<=s.length
 """
